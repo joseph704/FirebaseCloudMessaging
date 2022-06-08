bb15f52f4bcb634d1679c765706ec5e3a3f8aa3e
차요셉 <jscha@raoncorp.com>
2022-06-08 21:53:58 +0900

    fcm 코드 추가

---
+++ Pods/Target Support Files/GoogleDataTransport/GoogleDataTransport-umbrella 2.h
@@ -0,0 +1,29 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "GDTCORClock 2.h"
+#import "GDTCORClock.h"
+#import "GDTCORConsoleLogger.h"
+#import "GDTCOREndpoints.h"
+#import "GDTCOREvent 2.h"
+#import "GDTCOREvent.h"
+#import "GDTCOREventDataObject.h"
+#import "GDTCOREventTransformer.h"
+#import "GDTCORTargets 2.h"
+#import "GDTCORTargets.h"
+#import "GDTCORTransport.h"
+#import "GoogleDataTransport 2.h"
+#import "GoogleDataTransport.h"
+
+FOUNDATION_EXPORT double GoogleDataTransportVersionNumber;
+FOUNDATION_EXPORT const unsigned char GoogleDataTransportVersionString[];
+
--- Warning/AppDelegate.swift
+++ Warning/AppDelegate.swift
@@ -8,33 +8,44 @@
 import UIKit
 import Firebase
 import UserNotifications
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         UNUserNotificationCenter.current().delegate = self
         return true
     }
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         
         FirebaseApp.configure() // 파이어 베이스 초기화
         
-        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        Messaging.messaging().delegate = self
+        // [END set_messaging_delegate]
         
-        UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { _, error in
-            print("ERROR, Request Notifications Authorization: \(error.debugDescription)")
+        UNUserNotificationCenter.current().delegate = self
+        
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.sound, .alert, .badge]) { (granted, error) in
+            if (error == nil) {
+                print("AngelNet:::requestAuthorizationWithOptions")
+            }
+            if granted {
+                // update application settings
+                DispatchQueue.main.async {
+                    application.registerForRemoteNotifications()
+                }
+            }
         }
-        
-        application.registerForRemoteNotifications()
         
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -46,12 +57,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+}
 
-
+extension AppDelegate: MessagingDelegate {
+    // [START refresh_token]
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+    
+    // [END refresh_token]
 }
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.list, .banner, .badge, .sound])
-    }
+            print("AngelNet:::willPresent => \(notification.debugDescription)")
+
+            if #available(iOS 14.0, *) {
+                completionHandler([.banner, .list, .sound, .badge])
+            } else {
+                completionHandler([.alert, .sound, .badge])
+            }
+        }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+            print("AngelNet:::didReceive")
+
+            print(response)
+            completionHandler()
+        }
 }
